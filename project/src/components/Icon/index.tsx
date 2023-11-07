44c6466bd62797acd44b24184867e5f505cc7b8e
Samet Karaibryamov <einismotic@gmail.com>
2023-11-07 17:57:05 +0100

    Shortening of large number displays

--- project/src/components/Icon/index.tsx
+++ project/src/components/Icon/index.tsx
@@ -1,6 +1,18 @@
 import { OBJECT_SPECS } from 'objectSpecs'
 import styles from './icon.module.css'
 
+const BREAKPOINTS = [
+  [1000, 'K'],
+  [1000000, 'M'],
+] as const
+
+const shortenAmount = (v: number) => {
+  const bp = BREAKPOINTS.findLast(bp => bp[0] < v)
+  if (!bp) return v
+
+  return `${(v / bp[0]).toFixed(1)}${bp[1]}`
+}
+
 export const Icon = ({
   name,
   amount,
@@ -8,16 +20,16 @@ export const Icon = ({
   name?: ToolType | null
   amount?: string | number | null
 }) => {
-  const hasAmount = amount || amount === 0
+  const amountDisplay = (amount || amount === 0) && shortenAmount(+amount)
 
   return (
     <div className={styles.icon}>
       {name
         ? (<>
           <img src={name && OBJECT_SPECS[name].icon} />
-          {hasAmount && <div className={styles.amount}>{amount}</div>}
+          {amountDisplay && <div className={styles.subscript}>{amountDisplay}</div>}
         </>)
-        : (hasAmount && <div className={styles.text}>{amount}</div>)
+        : (amountDisplay && <div className={styles.text}>{amountDisplay}</div>)
       }
     </div>
   )
